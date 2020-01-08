7cda896a05a0540ea679032872125fdd0d5e6cba
lxnxbnq <yuanddmail@163.com>
2020-01-08 15:20:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -69,3 +69,16 @@ export const selectText = (textbox, startIndex, endIndex) => {
   }
   textbox.focus();
 }
+
+// 转义字符
+export function htmlDecodeByRegExp (str){
+  let s = "";
+  if(str.length == 0) return "";
+  s = str.replace(/&amp;/g,"&");
+  s = s.replace(/&lt;/g,"<");
+  s = s.replace(/&gt;/g,">");
+  s = s.replace(/&nbsp;/g," ");
+  s = s.replace(/&#39;/g,"\'");
+  s = s.replace(/&quot;/g,"\"");
+  return s;
+}
